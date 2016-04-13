91455ad3c62fca780ff6e6e6471be73b4b0d2399
InhoChoi <revaer@naver.com>
2016-04-13 20:52:18 +0900

    config 이름 추가

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var config = require('./config');
 global.models = require('./models/models.js');
 
 var routes = require('./routes/index');
@@ -31,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret : "alldoor",
+  secret : config.Name,
   resave : true,
   saveUninitialized: true
 }))
