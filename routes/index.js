6674ad2fa673073bf97ed3908872c3167b593e3d
InhoChoi <revaer@naver.com>
2016-04-14 16:04:21 +0900

    홈페이지 템플릿 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('../controller/passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',{});
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('page-login',{});
+});
+
+router.post('/login',passport.authenticate('local',{ successRedirect: '/', failureRedirect: '/login' }));
 
 module.exports = router;
