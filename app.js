90b25d6f27b212a322a43aff849a63bde40541e2
InhoChoi <revaer@naver.com>
2016-04-18 13:23:29 +0900

    multer formdata parser 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer')();
 
 var config = require('./config');
 global.models = require('./models/models.js');
@@ -32,6 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer.array());
 
 app.use(session({
   secret : config.Name,
