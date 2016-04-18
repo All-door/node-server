e6d853b23877e0de0cf163a8cb69d11aa3a7da00
InhoChoi <revaer@naver.com>
2016-04-18 13:29:07 +0900

    multer 기능 주석처리

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(multer.array());
+// app.use(multer.array());
 
 app.use(session({
   secret : config.Name,
