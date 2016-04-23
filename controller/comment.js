1b1688cf3c48a8edaee04faeec694d2c2a546a6a
InhoChoi <revaer@naver.com>
2016-04-23 17:46:47 +0900

    comment.js 소스정리

--- controller/comment.js
+++ controller/comment.js
@@ -15,18 +15,19 @@ module.exports = {
       return;
     }
 
-    Room.findOne({ _id : room_id})
-        .then((doc)=>{
-          if( doc == null){
-            callback("공간 정보가 존재하지 않습니다",null);
-          }else {
-            new Comment({
-              room_id : room_id,
-              user_id : user_id,
-              content : content
-            }).save(callback);
-          }
-        })
+    Room
+    .findOne({ _id : room_id})
+    .then((doc)=>{
+      if( doc == null){
+        callback("공간 정보가 존재하지 않습니다",null);
+      }else {
+        new Comment({
+          room_id : room_id,
+          user_id : user_id,
+          content : content
+        }).save(callback);
+      }
+    });
   },
   'UpdateComment' : (comment,callback)=>{
     callback = callback || ()=>{};
@@ -40,16 +41,17 @@ module.exports = {
       return;
     }
 
-    Comment.findOne({ _id : comment_id, user_id : user_id })
-           .then((doc)=>{
-             if(doc == null){
-               callback("존재하지 않는 코멘트입니다.",null);
-             }else{
-               let now = new Date();
-               Comment.update({ _id : comment_id }, { content : content, updatedAt : now })
-                      .exec(callback);
-             }
-           });
+    Comment
+    .findOne({ _id : comment_id, user_id : user_id })
+    .then((doc)=>{
+       if(doc == null){
+         callback("존재하지 않는 코멘트입니다.",null);
+       }else{
+         let now = new Date();
+         Comment.update({ _id : comment_id }, { content : content, updatedAt : now })
+                .exec(callback);
+       }
+     });
   },
   'RemoveComment' : (user_id,comment_id,callback)=>{
     callback = callback || ()=>{};
@@ -59,14 +61,18 @@ module.exports = {
       return;
     }
 
-    Comment.findOne({ _id : comment_id, user_id : user_id })
-           .then((doc)=>{
-             if( doc == null){
-               callback("존재하지 않는 코멘트입니다.",null);
-             }else{
-               Comment.findOne({ _id : comment_id}).remove().exec(callback);
-             }
-           });
+    Comment
+    .findOne({ _id : comment_id, user_id : user_id })
+    .then((doc)=>{
+       if( doc == null){
+         callback("존재하지 않는 코멘트입니다.",null);
+       }else{
+         Comment
+         .findOne({ _id : comment_id})
+         .remove()
+         .exec(callback);
+       }
+     });
   },
   'GetCommentsByRoomId' : (offset,limit,room_id,callback)=>{
     callback = callback || ()=>{};
@@ -77,12 +83,13 @@ module.exports = {
       callback("입력 데이터를 확인해주세요.",null);
       return;
     }
-    Comment.find({ room_id : room_id })
-           .sort({ createdAt : -1 })
-           .skip(offset)
-           .limit(limit)
-           .then((docs)=>{
-             callback(null,docs);
-           });
+    Comment
+    .find({ room_id : room_id })
+    .sort({ createdAt : -1 })
+    .skip(offset)
+    .limit(limit)
+    .then((docs)=>{
+      callback(null,docs);
+    });
   }
 };
