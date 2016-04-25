48e7e1bada91942881cecf916ef2498843db4a9b
InhoChoi <revaer@naver.com>
2016-04-25 23:57:05 +0900

    response status 에러 수정

--- controller/response.js
+++ controller/response.js
@@ -1,39 +1,45 @@
 module.exports = {
   'Error' : (res,err)=>{
-    res.json({
+    res.header('Access-Control-Allow-Origin', '*');
+    res.status(400).json({
       "status" : 400,
       "meessage" : err
-    }).status(200);
+    });
   },
   'AuthFail' : (res)=>{
-    res.json({
+    res.header('Access-Control-Allow-Origin', '*');
+    res.status(401).json({
       "status" : 401,
       "message" : "인증되지 않은 접근입니다."
-    }).status(401);
+    });
   },
   'User' : (res,user)=>{
-    res.json({
+    res.header('Access-Control-Allow-Origin', '*');
+    res.status(200).json({
       "status" : 200,
       "user" : user
-    }).status(200);
+    });
   },
   'Data' : (res,data)=>{
-    res.json({
+    res.header('Access-Control-Allow-Origin', '*');
+    res.status(200).json({
       "status" : 200,
       "data" : data
-    }).status(200);
+    });
   },
   'Message' : (res,message)=>{
-    res.json({
+    res.header('Access-Control-Allow-Origin', '*');
+    res.status(200).json({
       "status" : 200,
       "message" : message
-    }).status(200);
+    });
   },
   'Login' : (res,user)=>{
-    res.json({
+    res.header('Access-Control-Allow-Origin', '*');
+    res.status(200).json({
       "status" : 200,
       "message" : "로그인에 성공했습니다",
       "user" : user
-    }).status(200);
+    });
   }
 };
