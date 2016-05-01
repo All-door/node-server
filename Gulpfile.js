e188791f2c04f05ac72afe43688149a867b59e3d
InhoChoi <revaer@naver.com>
2016-05-02 00:20:17 +0900

    build system, gulp 설정

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,13 @@
 'use strict'
 let gulp = require('gulp');
+let minify = require('gulp-minify');
+let uglify = require('gulp-uglify');
 
 gulp.task('dist',()=>{
+  gulp.src('./public/js/*.js')
+  .pipe(uglify())
+  .pipe(minify())
+  .pipe(gulp.dest('./public/dist'));
 });
 
 gulp.task('default',['dist']);
