e83ce95ebcd477c895ca44bc6dfb79eacf52cd6b
InhoChoi <revaer@naver.com>
2016-05-12 12:24:11 +0900

    Revert "API: header 삭제"
    This reverts commit 8088f328abc31de6120a4391f08dfc4164070db5.

--- controller/response.js
+++ controller/response.js
@@ -1,36 +1,36 @@
 module.exports = {
   'Error' : (res,err)=>{
-    res.status(400).json({
+    res.header('Access-Control-Allow-Origin', '*').status(400).json({
       "status" : 400,
       "meessage" : err
     });
   },
   'AuthFail' : (res)=>{
-    res.status(401).json({
+    res.header('Access-Control-Allow-Origin', '*').status(401).json({
       "status" : 401,
       "message" : "인증되지 않은 접근입니다."
     });
   },
   'User' : (res,user)=>{
-    res.status(200).json({
+    res.header('Access-Control-Allow-Origin', '*').status(200).json({
       "status" : 200,
       "user" : user
     });
   },
   'Data' : (res,data)=>{
-    res.status(200).json({
+    res.header('Access-Control-Allow-Origin', '*').status(200).json({
       "status" : 200,
       "data" : data
     });
   },
   'Message' : (res,message)=>{
-    res.status(200).json({
+    res.header('Access-Control-Allow-Origin', '*').status(200).json({
       "status" : 200,
       "message" : message
     });
   },
   'Login' : (res,user)=>{
-    res.status(200).json({
+    res.header('Access-Control-Allow-Origin', '*').status(200).json({
       "status" : 200,
       "message" : "로그인에 성공했습니다",
       "user" : user
