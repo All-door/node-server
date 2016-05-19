e667b198b6e39e66f81a40ab7297022f1195e524
InhoChoi <revaer@naver.com>
2016-05-19 16:55:52 +0900

    WEB: 핸드폰 번호 변경 기능 구현

--- public/js/mypage/change.js
+++ public/js/mypage/change.js
@@ -29,7 +29,7 @@ var Change = (function(){
       },
       success : function(data){
         alert('비밀 번호가 변경되었습니다.');
-        location.href = "/";
+        location.href = "/mypage";
       },
       error : function(request,status,error){
         alert('기존 암호가 일치하지 않습니다.');
@@ -41,7 +41,6 @@ var Change = (function(){
   * MY PAGE CHANGE MODULE INIT
   */
   var init = function(){
-    console.log("MY PAGE CHANGE MODULE INIT");
     $('#change-submit').click(onClick_submit);
   };
 
@@ -51,3 +50,64 @@ var Change = (function(){
 })();
 
 $(document).ready(Change.init());
+
+
+var Change_PhoneNumber = (function(){
+  var PhoneNumberRegex = /^\d{3}-\d{3,4}-\d{4}$/;
+
+  var getUserInformation = function(){
+    var url = "/api/user";
+
+    $.ajax({
+      url : url,
+      method : 'GET',
+      success : function(data){
+        Render(data.user);
+      },
+      error : function(request,status,error){
+        alert('로그인이 필요한 서비스입니다.');
+        location.href="/login";
+      }
+    });
+  };
+
+  var Render = function(data){
+    var phoneNumber = data.phoneNumber ? data.phoneNumber : '000-0000-0000';
+    $('#phoneNumber').val(phoneNumber);
+  };
+
+  var onClick_submit_phoneNumber = function(){
+    var phoneNumber = $('#change-phoneNumber').val();
+
+    if( PhoneNumberRegex.test(phoneNumber) == false){
+      alert('핸드폰 번호가 잘못되었습니다.');
+      return;
+    }
+
+    $.ajax({
+      url : '/api/user',
+      method : 'PUT',
+      dataType : 'json',
+      data : {
+        phoneNumber : phoneNumber
+      },
+      success : function(data){
+        alert('핸드폰 번호가 변경되었습니다.');
+        location.href = "/mypage";
+      },
+      error : function(request,status,error){
+        alert('서버 오류입니다. 관리자에게 문의해주세요');
+        location.href = "/";
+      }
+    })
+  }
+  var init = function(){
+    getUserInformation();
+    $('#change-submit-phoneNumber').click(onClick_submit_phoneNumber);
+  };
+
+  return {
+    init : init
+  };
+})();
+$(document).ready(Change_PhoneNumber.init());
