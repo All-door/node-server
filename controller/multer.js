357be648bfb3961ec70255c5f631121833eba282
InhoChoi <revaer@naver.com>
2016-05-27 13:47:29 +0900

    API: Multer 파일 저장시 확장명 추가

--- controller/multer.js
+++ controller/multer.js
@@ -2,12 +2,14 @@
 const config = require('../config');
 const multer = require('multer');
 const shortid = require('shortid');
+const path = require('path');
+
 const storage = multer.diskStorage({
   'destination' : (req, file, cb) => {
     cb(null, config.ImagePath);
   },
   'filename' : (req, file, cb) => {
-    cb(null, file.fieldname + '-'+ shortid() + '-' + Date.now());
+    cb(null, file.fieldname + '-'+ shortid() + '-' + Date.now()+'.'+path.extname(file.originalname));
   }
 });
 const upload = multer({ storage : storage });
