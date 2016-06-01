241c62753ba2971001b92804cb4d90f2241b20fa
InhoChoi <revaer@naver.com>
2016-06-01 17:49:25 +0900

    Config : Config 이미지 주소 수정

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 const config = {
   Name : 'alldoor',
   MongoDB : 'mongodb://localhost/alldoor',
-  ImagePath : '/root/images',
+  ImagePath : process.env.ALL_DOOR_IMAGE_PATH ? process.env.ALL_DOOR_IMAGE_PATH : '/root/images',
   Port : '3000',
   RedisOption : {
 
   }
 };
-
+console.log(process.env.ALL_DOOR_IMAGE_PATH);
 module.exports = config;
