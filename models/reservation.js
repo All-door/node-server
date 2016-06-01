bda74e722d756f86c4ce1dfbc020eefd18086749
InhoChoi <revaer@naver.com>
2016-06-01 22:05:57 +0900

    Model: Revert commit 예약 스키마 커밋 취소 b9fb5491e

--- models/reservation.js
+++ models/reservation.js
@@ -2,6 +2,7 @@
 let shortid = require('shortid');
 let mongoose = require('mongoose');
 let timeRegex = /^([0-9]|0[0-9]|1[0-9]|2[0-4]):[0][0]$/;
+let statusEnum = ['예약 완료','예약 취소','관리자 취소'];
 let TimeValidator = (v)=>{
   return timeRegex.test(v);
 };
@@ -25,6 +26,10 @@ let ReservationSchema = mongoose.Schema({
   end_time : { type : String, default : "00:00", validate : {
     validator : TimeValidator,
     message : '시간 정보 형식이 잘못되었습니다 (HH:MM 1시간 단위)'
+  }},
+  status : { type : String, default : '예약 완료', required : [ true, "예약 상태 정보가 필요합니다." ], validate : {
+     validator : statusValiadtor,
+     message : '예약 상태 정보가 잘못되었습니다.'
   }},
   password : { type : String, required : [ true, "비밀번호 정보가 필요합니다."]},
   room_title : { type : String, required : [ true, "공간 제목 정보가 필요합니다."]},
