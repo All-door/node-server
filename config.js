1b4254cac98a430d00bb98fb68632af054f5ebfe
InhoChoi <revaer@naver.com>
2016-06-05 21:46:33 +0900

    Config : Config file 수정

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict'
 const config = {
-  Name : 'alldoor',
-  MongoDB : 'mongodb://localhost/alldoor',
+  Name : process.env.ALL_DOOR_NAME ? process.env.ALL_DOOR_NAME : 'alldoor',
+  MongoDB : process.env.ALL_DOOR_MONGODB ? process.env.ALL_DOOR_MONGODB : 'mongodb://localhost/alldoor',
   ImagePath : process.env.ALL_DOOR_IMAGE_PATH ? process.env.ALL_DOOR_IMAGE_PATH : '/root/images',
-  Port : '3000',
+  Port : process.env.ALL_DOOR_PORT ? process.env.ALL_DOOR_PORT : '3000',
   RedisOption : {
 
   }
